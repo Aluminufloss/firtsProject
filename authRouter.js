2a4dccebade3e9a386a77f5a4cd8d2ff1cdfcc0a
Nazar Ilchenko <nilchenko2020@gmail.com>
2022-11-19 18:19:26 +0300

    updated the function of viewing users, added JWT tokens

--- authRouter.js
+++ authRouter.js
@@ -2,13 +2,14 @@ const Router = require('express');
 const router = new Router();
 const controller = require("./authController");
 const {check} = require("express-validator");
-const authMiddleware = require('./middlewares/authMiddlewares');
+const authMiddleware = require('./middlewares/authMiddleware');
+const roleMiddleware = require('./middlewares/roleMiddleware');
 
 router.post('/registation', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
     check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
 ], controller.registation);
 router.post('/login', controller.login);
-router.get('/users', authMiddleware, controller.getUsers);
+router.get('/users', roleMiddleware(['USER', 'ADMIN']), controller.getUsers);
 
 module.exports = router;
